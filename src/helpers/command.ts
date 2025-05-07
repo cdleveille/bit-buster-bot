de3a0d717e5af680040b053002e81842afe08a7a
Chris Leveille <cdleveille@gmail.com>
2025-05-06 23:51:13 -0400

    fixed wording

--- src/helpers/command.ts
+++ src/helpers/command.ts
@@ -6,7 +6,7 @@ import { Config } from "@helpers";
 export const applicationCommands = Object.values(commands);
 
 export const registerApplicationCommands = async (applicationId: string) => {
-	console.log("Registering slash commands...");
+	console.log("Registering application commands...");
 	const body = applicationCommands.map(cmd => cmd.definition.toJSON());
 	const DiscordApi = new REST({ version: "10" }).setToken(Config.BOT_TOKEN);
 	await DiscordApi.put(Routes.applicationCommands(applicationId), { body });
